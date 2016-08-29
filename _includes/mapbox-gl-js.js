80c1eec11d0a060819e9b967e744c5f3f340e51e
Rob | Labs <roblabs@users.noreply.github.com>
2016-08-28 18:12:00 -0700

    mapbox-gl-js/v0.23.0

--- _includes/mapbox-gl-js.js
+++ _includes/mapbox-gl-js.js
@@ -1,5 +1,5 @@
-<script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.21.1/mapbox-gl.js'></script>
-<link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.21.1/mapbox-gl.css' rel='stylesheet' />
+<script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.23.0/mapbox-gl.js'></script>
+<link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.23.0/mapbox-gl.css' rel='stylesheet' />
 <script>
   mapboxgl.accessToken = 'pk.eyJ1Ijoicm9ibGFicyIsImEiOiJwVlg0cnZnIn0.yhekddtKwZohGoORaWjqIw';
 </script>
