f97a43e8fef741e8b0b2758944ed70832f605095
Rob | Labs <roblabs@users.noreply.github.com>
2018-04-19 11:40:17 -0700

    /sanj /flag

---
+++ styles/styles.sh
@@ -0,0 +1,25 @@
+function updateStyleTemplates() {
+  SOURCE_STYLE=../fstopo/style.json
+  SOURCE_TEMPLATE=template.json
+
+  # extract the layers array
+  cat $SOURCE_STYLE | json layers > layers.array
+
+
+  # Create a new JSON from the layers array
+  #  TODO - might be a better way to generate JSON...
+  echo { \"layers\": > layers.json
+  cat layers.array >>  layers.json
+  echo } >>            layers.json
+
+  # Join the layers with the templates
+  join-json -f \
+    -i $SOURCE_TEMPLATE \
+    -i layers.json \
+    -o out.json
+
+    cat out.json | json -2 > style.json
+
+  # clean up
+  rm layers.array layers.json out.json
+}
