c49a2b2e63aa28f6143a03d76a9a28e82889da57
Rob | Labs <roblabs@users.noreply.github.com>
2018-10-30 22:03:14 -0700

    /usgs

--- go.sh
+++ go.sh
@@ -1,3 +1,12 @@
 
 # bundle exec jekyll serve
 bundle exec jekyll serve --ssl-key ssl/localhost.key --ssl-cert ssl/localhost.crt
+
+
+# export JEKYLL_VERSION=latest
+# docker run --rm \
+#  --volume="$PWD:/srv/jekyll" \
+#  --volume="$PWD/vendor/bundle:/usr/local/bundle" \
+#  -it jekyll/jekyll:$JEKYLL_VERSION \
+#  jekyll serve #       --ssl-key ssl/localhost.key --ssl-cert ssl/localhost.crt 
+  # jekyll build
