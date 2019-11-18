b9ba47cb71ee6630df841076bf1ca918ed244689
Rob | Labs <roblabs@users.noreply.github.com>
2019-11-18 14:05:18 -0800

    /observablehq/map-grids-scales-in-svg.html

--- go.sh
+++ go.sh
@@ -1,12 +1,18 @@
+# super easy
+# https://kristofclaes.github.io/2016/06/19/running-jekyll-locally-with-docker/
+docker-compose up
+
 
 # bundle exec jekyll serve
-bundle exec jekyll serve --ssl-key ssl/localhost.key --ssl-cert ssl/localhost.crt
+# bundle exec jekyll serve --ssl-key ssl/localhost.key --ssl-cert ssl/localhost.crt
 
 
-# export JEKYLL_VERSION=latest
-# docker run --rm \
-#  --volume="$PWD:/srv/jekyll" \
+export JEKYLL_VERSION=latest
+docker run --rm \
+ --volume="$PWD:/srv/jekyll" \
+ -it jekyll/jekyll:$JEKYLL_VERSION \
+ jekyll build
+ # jekyll serve #       --ssl-key ssl/localhost.key --ssl-cert ssl/localhost.crt
+
+
 #  --volume="$PWD/vendor/bundle:/usr/local/bundle" \
-#  -it jekyll/jekyll:$JEKYLL_VERSION \
-#  jekyll serve #       --ssl-key ssl/localhost.key --ssl-cert ssl/localhost.crt 
-  # jekyll build
