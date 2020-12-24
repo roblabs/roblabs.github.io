3b8727ac65fbe75d5fbffaf73e7b46fe8ec51f15
RobLabs.com <roblabs@users.noreply.github.com>
2020-12-24 07:20:15 -0800

    Now with 3.14159% more awesome

--- go.sh
+++ go.sh
@@ -1,3 +1,8 @@
+# be sure to clean up any old `_site`
+rm .jekyll-metadata
+rm -rf tmp/_site/www
+rm -rf tmp/_site
+
 # super easy
 # https://kristofclaes.github.io/2016/06/19/running-jekyll-locally-with-docker/
 docker-compose up
@@ -6,11 +11,10 @@ docker-compose up
 # bundle exec jekyll serve
 # bundle exec jekyll serve --ssl-key ssl/localhost.key --ssl-cert ssl/localhost.crt
 
-# be sure to clean up any old `_site`
-rm -rf tmp_site
 
-#export JEKYLL_VERSION=jekyll/jekyll:latest  # d11121e6548e
-export JEKYLL_VERSION=8e011d7b15e0
+export JEKYLL_VERSION=jekyll/jekyll:latest  # d11121e6548e
+#export JEKYLL_VERSION=8e011d7b15e0
+
 docker run --rm \
  --volume="$PWD:/srv/jekyll" \
  -it $JEKYLL_VERSION \
