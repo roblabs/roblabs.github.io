f09608bc4dc6fe22077ca369ecea9bba5f4df3b4
RobLabs.com <roblabs@users.noreply.github.com>
2022-04-20 12:40:42 -0700

    Split JavaScript Includes to be configured in the Page YAML

---
+++ _includes/livephotoskit.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.onload = function() {
+    console.log("Use the LivePhotosKit JS library to play Live Photos on your web pages.\n\thttps://developer.apple.com/documentation/livephotoskitjs");
+}
+script.async = true;
+script.charset = 'utf-8';
+script.src = 'https://cdn.apple-livephotoskit.com/lpk/1/livephotoskit.js';
+head.appendChild(script);
+</script>
---
+++ _includes/twitter.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.onload = function() {
+    console.log("Embedded Tweets bring your pick of content from Twitter into your website articles.\n\thttps://developer.twitter.com/en/docs/twitter-for-websites/embedded-tweets/overview");
+}
+
+script.async = true;
+script.src = 'https://platform.twitter.com/widgets.js';
+script.charset = 'utf-8';
+head.appendChild(script);
+</script>
